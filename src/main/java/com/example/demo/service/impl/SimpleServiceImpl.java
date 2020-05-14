0ec92a899c6e989506181d3aa03e1af0d5838ac0
Чухлебов Андрей Васильевич <Chukhlebov.A.Va@sberbank.ru>
2020-05-14 13:32:42 +0300

    add Dockerfile

--- src/main/java/com/example/demo/service/impl/SimpleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SimpleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.service.SimpleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -8,8 +10,12 @@ import java.util.List;
 
 @Service
 public class SimpleServiceImpl implements SimpleService {
+
+    private static Logger logger =  LoggerFactory.getLogger(SimpleServiceImpl.class);
+
     @Override
     public List<String> getStrings() {
-        return Arrays.asList("1","2","3");
+        logger.info("invoke mhetod");
+        return Arrays.asList("1", "2", "3");
     }
 }
